f59f5897bf875afdda768903016203477626ddb9
Eric P. Katz <ericpkatz@gmail.com>
2017-06-06 19:16:36 -0400

    add enzyme

--- src/components/Home-test.js
+++ src/components/Home-test.js
@@ -1,17 +1,11 @@
 import Home from './Home';
-var Utils = require('react-dom/test-utils');
 import React, { Component } from 'react';
+import { mount } from 'enzyme';
 
-class Wrapper extends Component{
-  render(){
-    return this.props.children;
-  }
-}
-
-describe('home', function () {
-  it('renders without problems', function () {
-    const wrapper = Utils.renderIntoDocument(<Wrapper><Home /></Wrapper>);
-    const div = Utils.findRenderedDOMComponentWithTag(wrapper, 'div');
-    expect(div).to.be.ok;
+describe('Home', ()=> {
+  it('renders', ()=> {
+    const wrapper = mount(<Home fizz='buzz'/>);
+    const span = wrapper.find('div');
+    expect(wrapper).to.be.ok;
   });
 });
